0de80ee1e08da4976adc647280084ba8a97dc9fb
Zapaxe <rajithbb409@gmail.com>
2026-07-25 21:36:07 +0530

    Bug Fix: Visit Variable Initializer Expressions During Usage Analysis

--- internal/codegen/c/analysis/visit_var.go
+++ internal/codegen/c/analysis/visit_var.go
@@ -4,4 +4,5 @@ import "github.com/azin-lang/Azin/internal/ast"
 
 func (a *Analyzer) visitVar(fn string, stmt *ast.VarStmt) {
 	a.registerVariable(fn, stmt.Name.Value)
+	a.visitExpr(fn, stmt.Value)
 }
